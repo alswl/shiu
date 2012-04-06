788897473167b9bdf4218b668a9409c10d74acc6
alswl <alswlx@gmail.com>
2012-04-06 17:10:10 +0800

    加入图标 / 和看书无关的基础服务搭建起来了

--- app/js/sixu.js
+++ app/js/sixu.js
@@ -5,8 +5,33 @@ jQuery.extend({
 })
 var app = {};
 
-if ($.isIphone()) {
-	$('#download').show();
-} else {
+if (!$.isIphone()) {
 	$('#noiphone').show();
+} else {
+    if (!window.navigator.standalone) {
+		$('#download').show();
+		var appCache = window.applicationCache;
+		appCache.ondownloading = function () {
+			window.progresscount = 0
+		};
+        appCache.onprogress = function (e) {
+            var percent = "";
+            if (e && e.lengthComputable) {
+                percent = Math.round(100 * e.loaded / e.total)
+            } else {
+                percent = Math.round(100 * (++progresscount / 8)) 
+            }
+			$("#download .percent").text(percent);
+        };
+        appCache.oncached = function (e) {
+			$("#download .percent").text('100');
+            $("#download .tip").text("下载完成");
+            $("#download .complete").show()
+        };
+        appCache.onnoupdate = function () {
+			$("#download .percent").text('100');
+            $("#download .tip").text("下载完成");
+            $("#download .complete").show()
+        }
+	}
 }
