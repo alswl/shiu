78b073224313d78fa5f36425196efb58e85861d6
alswl <alswlx@gmail.com>
2012-04-11 12:11:26 +0800

    开发版用完整的 js / css

--- js/shiu.js
+++ js/shiu.js
@@ -1,5 +1,5 @@
 $(function() {
-	var DEBUG = false;
+	var DEBUG = true;
 	//var DEBUG_MODE = 'normal'; // normal / standalone
 	var DEBUG_MODE = 'standalone'; // normal / standalone
 	if (DEBUG && DEBUG_MODE == 'standalone') {
