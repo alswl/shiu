f3349ee506ea9709c990ae5d2d032e4a83680754
alswl <alswlx@gmail.com>
2012-04-11 12:41:39 +0800

    fix ls bug

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-java -jar ~/local/yuicompressor/yuicompressor-2.4.7.jar -o '.js$:.min.js' --type js `ls js/*[^m][^i][^n].js`
-java -jar ~/local/yuicompressor/yuicompressor-2.4.7.jar -o '.css$:.min.css' --type css `ls style/*[^m][^i][^n].css`
+java -jar ~/local/yuicompressor/yuicompressor-2.4.7.jar -o '.js$:.min.js' --type js `ls -1 js/*.js | sed 's/.*min.js//'`
+java -jar ~/local/yuicompressor/yuicompressor-2.4.7.jar -o '.css$:.min.css' --type css `ls -1 style/*.css | sed 's/.*min.css//'`
 
 java -jar ~/local/yuicompressor/yuicompressor-2.4.7.jar -o '.js$:.min.js' --type js `ls */book.js`
