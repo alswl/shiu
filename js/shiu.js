24de8facb8010827257533079b22e4a00988b905
alswl <alswlx@gmail.com>
2012-04-11 10:18:49 +0800

    修正缓存 js 读取机制

--- js/shiu.js
+++ js/shiu.js
@@ -1,6 +1,6 @@
 var DEBUG = false;
-//var DEBUG_MODE = 'normal'; // normal / standalone
-var DEBUG_MODE = 'standalone'; // normal / standalone
+var DEBUG_MODE = 'normal'; // normal / standalone
+//var DEBUG_MODE = 'standalone'; // normal / standalone
 if (DEBUG && DEBUG_MODE == 'standalone') {
 	window.navigator.standalone = true;
 }
